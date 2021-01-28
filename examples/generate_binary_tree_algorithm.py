fdea805b08ad9203d608998aaa79ce5670d48bc1
Ashish Shevale <shevaleashish@gmail.com>
2021-01-28 13:38:24 +0530

    Created example file for binary tree algorithm

---
+++ examples/generate_binary_tree_algorithm.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+from src.maze_manager import MazeManager
+from src.maze import Maze
+
+
+if __name__ == "__main__":
+
+    # create a maze manager to handle all operations
+    manager = MazeManager()
+
+    # now create a maze using the binary tree method
+    maze_using_btree = Maze(10, 10, algorithm="bin_tree")
+
+    # add this maze to the maze manager
+    maze_using_btree = manager.add_existing_maze(maze_using_btree)
+
+    # show the maze
+    manager.show_maze(maze_using_btree.id)
+
+    # show how the maze was generated
+    manager.show_generation_animation(maze_using_btree.id)
